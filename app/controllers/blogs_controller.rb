0ca6eaa24dd2f19a27e3ccc7c6702b9a9101e308
shiho-h <sisopiku@gmail.com>
2020-06-13 00:53:06 +0900

    finish4-5

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,34 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  	@blog = Blog.find(params[:id])
+  end
+
+  def index
+  	@blogs = Blog.all
+  end
+
+
+  def new
+  	@blog = Blog.new
+  end
+
+  def create
+  	  blog = Blog.new(blog_params)
+  	  blog.save
+  	  redirect_to blog_path(blog.id) #この行を修正
+  end
+
+
+  def edit
+  end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
+  end
+
+
+end
